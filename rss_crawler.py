8faa12023d5eb2712699d7595ca6627b7279228d
acesur <sures.chaudhary0@gmail.com>
2025-03-08 01:59:06 +0000

    Update with the crawng sources

---
+++ rss_crawler.py
@@ -0,0 +1,10 @@
+import feedparser
+import pandas as pd
+import time
+import random
+import os
+import rs
+from bs4 import BeautifulSoup
+import requests
+import date import datetime
+from urllib.parse import urlparse
