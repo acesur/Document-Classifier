e9ff53a65f4245da295a57990ec3fed3542abf49
unknown <sures.chaudhary0@gmail.com>
2025-03-09 09:45:15 +0545

    Sorting Models based upon the scores

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    text = request.form['text']
+    text = request.form.get('text', '')
+
+    if not text:
+        return render_template('result.html', error="No text provided!")
 
     processed_text = preprocess_text(text)
     text_tfidf = vectorizer.transform([processed_text])
@@ -24,8 +27,8 @@ def predict():
     lr_prediction = lr_model.predict(text_tfidf)[0]
     lr_probabilities = lr_model.predict_proba(text_tfidf)[0]
 
-    rf_prediction = lr_model.predict(text_tfidf)[0]
-    rf_probabilities = lr_model.predict_proba(text_tfidf)[0]
+    rf_prediction = rf_model.predict(text_tfidf)[0]
+    rf_probabilities = rf_model.predict_proba(text_tfidf)[0]
 
     categories = nb_model.classes_
 
@@ -33,9 +36,24 @@ def predict():
     lr_prob_dict = {categories[i]: lr_probabilities[i] for i in range(len(categories))}
     rf_prob_dict = {categories[i]: rf_probabilities[i] for i in range(len(categories))}
 
-    return render_template('result.html', nb_prediction = nb_prediction, nb_prob_dict = nb_prob_dict,
-                           lr_prediction=lr_prediction, lr_prob_dict=lr_prob_dict, 
-                           rf_prediction =rf_prediction, rf_prob_dict=rf_prob_dict)
+    model_results = {
+        "Naive Bayes": {"prediction": nb_prediction, "scores": nb_prob_dict},
+        "Logistic Regression": {"prediction": lr_prediction, "scores": lr_prob_dict}, 
+        "Random Forest": {"prediction":rf_prediction, "scores": rf_prob_dict}
+    }
+
+    sorted_models = sorted(
+        model_results.items(),
+        key=lambda item: max(item[1]["scores"].values()),
+        reverse=True
+    )
+
+    return render_template('result.html', 
+                           sorted_models=sorted_models,
+                           nb_prob_dict=nb_prob_dict,
+                           lr_prob_dict=lr_prob_dict,
+                           rf_prob_dict=rf_prob_dict
+                           )
 
 if __name__ == "__main__":
     app.run(debug=True)
